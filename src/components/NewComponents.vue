57904a681a04e5d24276281e3ec80951c1ccd8ef
spacemonkey <spacemonkey925@gmail.com>
2020-10-12 09:53:53 +0900

    프리티어 , 새로운 컴포넌트 추가

---
+++ src/components/NewComponents.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ title }}
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  setup() {
+    const title = ref('title')
+    return {
+      title
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <NewComponents></NewComponents>
   </div>
 </template>
+
+<script>
+import NewComponents from '../components/NewComponents'
+export default {
+  components: {
+    NewComponents
+  }
+}
+</script>
+
+<style></style>script
